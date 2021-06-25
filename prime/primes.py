40e0e719f2be6927cb75ef120bc83e93c233d2bb
yoshiking <yoshiking0711@gmail.com>
2021-06-25 10:04:30 +0900

    Update primes

--- prime/primes.py
+++ prime/primes.py
@@ -57,6 +57,18 @@ def eratosthenes(n):
     
     return primes + list(nums)
 
+def faster_eratosthenes(n):
+    primes = []
+    is_prime = [True] * (n+1)
+    is_prime[0] = is_prime[1] = False
+
+    for i in range(2, n+1):
+        if is_prime[i]:
+            primes.append(i)
+            for j in range(2*i, n+1, i):
+                is_prime[j] = False
+    return primes
+
 # 2 ~ n までの素数を列挙
 n = int(sys.argv[1])
 
