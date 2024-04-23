d2d4905ad9e4972f47194538be6d29a85f0eee97
yoshiking <yoshiking0711@gmail.com>
2024-04-23 23:32:46 +0900

    Add 1-iterator

---
+++ java_design_patterns/1-iterator/Main.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        BookShelf bookShelf = new BookShelf(4);
+        bookShelf.appendBook(new Book("Around the World in 80 days"));
+        bookShelf.appendBook(new Book("Bible"));;
+        bookShelf.appendBook(new Book("Cinderella"));
+        bookShelf.appendBook(new Book("Daddy-Long-Legs"));
+        bookShelf.appendBook(new Book("Eeeeee"));
+        Iterator it = bookShelf.iterator();
+        while (it.hasNext()) {
+            Book book = (Book)it.next();
+            System.out.println(book.getName());
+        }
+    }
+}
