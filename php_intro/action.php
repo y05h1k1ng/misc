3495e1afe69a2aa7a6a2cb02694660f3598644b0
yoshiking <yoshiking0711@gmail.com>
2024-05-15 02:33:40 +0900

    Update

---
+++ php_intro/action.php
@@ -0,0 +1,2 @@
+hello, <?php echo htmlspecialchars($_POST['name']);?>!!<br>
+you are <?php echo (int)$_POST['age']; ?> old.
--- php_intro/hello.php
+++ php_intro/hello.php
@@ -1,11 +1,40 @@
 <!DOCTYPE html>
+<head>
+    <style>
+        .center-text {
+            text-align: center;
+        }
+    </style>
+</head>
 <html>
     <head>
         <title>PHP Test</title>
     </head>
+    <form action="action.php" method="POST">
+        <label for="name">名前：</label>
+        <input name="name" id="name" type="text">
+
+        <label for="age">年齢：</label>
+        <input name="age" id="age" type="number">
+
+        <button type="submit">Submit</button>
+    </form>
     <body>
-        <?php echo '<p>Hello World</p>'; ?>
-        <?php phpinfo(); ?> 
-        <?php echo $_SERVER['HTTP_USER_AGENT'] ?>
+        <?php
+        if (str_contains($_SERVER['HTTP_USER_AGENT'], 'Firefox')) {
+        ?>
+        <h3>You use Firefox.</h3>
+        <?php
+        } else {
+        ?>
+        <div class="center-text">
+            <pre>
+                What's??<br>
+                <?php echo $_SERVER['HTTP_USER_AGENT']; ?>
+            </pre>
+        </div>
+        <?php
+        }
+        ?>
     </body>
 </html>
